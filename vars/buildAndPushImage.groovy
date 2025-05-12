bac6b58fdbd70d27a1febb9abc3477210ccd85a6
Opara Bright <oparabrightchukz@yahoo.com>
2025-05-12 12:06:50 +0100

    Added content to buildAndPushImage.groovy

--- vars/buildAndPushImage.groovy
+++ vars/buildAndPushImage.groovy
@@ -1,1 +1,32 @@
+def call(Map config = [:]) {
+    // Default values if not provided in config
+    def dockerImage = config.dockerImage ?: 'default/image'
+    def tag = config.tag ?: 'latest'
+    def registry = config.registry ?: 'docker.io'
+    def credentialsId = config.credentialsId ?: 'docker_credentials_id'
+
+    stage('Building Docker Image') {
+        script {
+            docker.build("${dockerImage}:${tag}")
+        }
+    }
+
+    stage('Pushing Docker Image') {
+        withCredentials([usernamePassword(
+            credentialsId: credentialsId,
+            usernameVariable: 'DOCKER_USER',
+            passwordVariable: 'DOCKER_PASS'
+        )]) {
+            withEnv(["IMAGE_TAG=${dockerImage}:${tag}"]) {
+                script {
+                    sh '''
+                        echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+                        docker push "$IMAGE_TAG"
+                    '''
+                }
+            }
+        }
+    }
+}
+
 
